cd112ca4c5966f390997d356fc9eb0057ee9984d
Christoph Kolhoff <christoph.kolhoff@stud.uni-hannover.de>
2026-05-11 22:10:10 +0200

    Further application

--- generate_plans.cpp
+++ generate_plans.cpp
@@ -1,6 +1,11 @@
 #include<iostream>
 
+#include "GeneratePlan.h"
+
 int main(int argc, char* argv[]) {
+
+    GeneratePlan genPlan;
+    genPlan.printPlan(1, "./data_environment/plan1");
 
     return 0;
 }
