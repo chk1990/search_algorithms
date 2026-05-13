04b41c978cb56da0a0a67c3c1f1d8bd1780342dd
Christoph Kolhoff <christoph.kolhoff@stud.uni-hannover.de>
2026-05-13 18:31:21 +0200

    Last unit tests for class Grid2D

--- unit_tests/test_Grid2D.cpp
+++ unit_tests/test_Grid2D.cpp
@@ -5,6 +5,8 @@
 
 #include "Grid2D.h"
 
+// ----------------------------------------
+// Tests without offset
 struct ConstructorNoOffset : public testing::Test
 {
     Grid2D<float> *grid;
@@ -16,27 +18,6 @@ struct ConstructorNoOffset : public testing::Test
         float step = 2.0;
 
         grid = new Grid2D<float>(x, y, step);
-    }
-
-    void TearDown()
-    {
-        delete grid;
-    }
-};
-
-struct ConstructorWithOffset : public testing::Test
-{
-    Grid2D<float> *grid;
-
-    void SetUp()
-    {
-        float x = 10.0;
-        float y = 5.0;
-        float step = 2.0;
-        float xOff = -1.0;
-        float yOff = -2.0;
-
-        grid = new Grid2D<float>(x, y, step, xOff, yOff);
     }
 
     void TearDown()
@@ -85,19 +66,57 @@ TEST_F(ConstructorNoOffset, CoordinateTest2Y) {
     ASSERT_EQ(y, 8.0);
 }
 
-TEST_F(ConstructorNoOffset, CoordinateTest2XOut) {
+TEST_F(ConstructorNoOffset, CoordinateTest2XOut1) {
     Point2D<float> pt = grid->getCoordinates(10, 4);
     float x = pt.getX();
 
     ASSERT_EQ(x, 18.0);
 }
 
-TEST_F(ConstructorNoOffset, CoordinateTest2YOut) {
+TEST_F(ConstructorNoOffset, CoordinateTest2YOut1) {
     Point2D<float> pt = grid->getCoordinates(2, 5);
     float y = pt.getY();
 
     ASSERT_EQ(y, 8.0);
 }
+
+TEST_F(ConstructorNoOffset, CoordinateTest2XOut2) {
+    Point2D<float> pt = grid->getCoordinates(-1, 4);
+    float y = pt.getY();
+
+    ASSERT_EQ(y, 8.0);
+}
+
+TEST_F(ConstructorNoOffset, CoordinateTest2YOut2) {
+    Point2D<float> pt = grid->getCoordinates(2, -1);
+    float x = pt.getX();
+
+    ASSERT_EQ(x, 4.0);
+}
+
+// ----------------------------------------
+// Tests with offset
+
+struct ConstructorWithOffset : public testing::Test
+{
+    Grid2D<float> *grid;
+
+    void SetUp()
+    {
+        float x = 10.0;
+        float y = 5.0;
+        float step = 2.0;
+        float xOff = -1.0;
+        float yOff = -2.0;
+
+        grid = new Grid2D<float>(x, y, step, xOff, yOff);
+    }
+
+    void TearDown()
+    {
+        delete grid;
+    }
+};
 
 TEST_F(ConstructorWithOffset, GetWidth) {
     ASSERT_EQ(grid->getWidth(), 10.0);
@@ -109,6 +128,62 @@ TEST_F(ConstructorWithOffset, GetHeigth) {
 
 TEST_F(ConstructorWithOffset, GetStep) {
     ASSERT_EQ(grid->getStep(), 2.0);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest1X) {
+    Point2D<float> pt = grid->getCoordinates(0, 0);
+    float x = pt.getX();
+
+    ASSERT_EQ(x, -1);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest1Y) {
+    Point2D<float> pt = grid->getCoordinates(0, 0);
+    float y = pt.getY();
+
+    ASSERT_EQ(y, -2);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest2X) {
+    Point2D<float> pt = grid->getCoordinates(9, 4);
+    float x = pt.getX();
+
+    ASSERT_EQ(x, 17.0);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest2Y) {
+    Point2D<float> pt = grid->getCoordinates(9, 4);
+    float y = pt.getY();
+
+    ASSERT_EQ(y, 6.0);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest2XOut1) {
+    Point2D<float> pt = grid->getCoordinates(10, 4);
+    float x = pt.getX();
+
+    ASSERT_EQ(x, 17.0);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest2YOut1) {
+    Point2D<float> pt = grid->getCoordinates(2, 5);
+    float y = pt.getY();
+
+    ASSERT_EQ(y, 6.0);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest2XOut2) {
+    Point2D<float> pt = grid->getCoordinates(-1, 4);
+    float y = pt.getY();
+
+    ASSERT_EQ(y, 6.0);
+}
+
+TEST_F(ConstructorWithOffset, CoordinateTest2YOut2) {
+    Point2D<float> pt = grid->getCoordinates(2, -1);
+    float x = pt.getX();
+
+    ASSERT_EQ(x, 3.0);
 }
 
 int main(int argc, char* argv[]) {
--- unit_tests/test_Point2D.cpp
+++ unit_tests/test_Point2D.cpp
@@ -61,6 +61,5 @@ TEST(Test, SetY) {
 
 int main(int argc, char* argv[]) {
     testing::InitGoogleTest(&argc, argv);
-
     return RUN_ALL_TESTS();
 }
